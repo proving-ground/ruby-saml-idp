c7dfce0e39d2f684733e1893961b6c0c3e9c50b6
Travis Bailey <mail@travisbailey.com>
2013-06-12 16:37:14 -0400

    small change in hopes to fix signing thing...

--- lib/saml_idp/controller.rb
+++ lib/saml_idp/controller.rb
@@ -29,11 +29,11 @@ module SamlIdp
       @algorithm = algorithm
       if algorithm.is_a?(Symbol)
         @algorithm = case algorithm
-        when :sha256 then Digest::SHA256
-        when :sha384 then Digest::SHA384
-        when :sha512 then Digest::SHA512
+        when :sha256 then OpenSSL::Digest::SHA256
+        when :sha384 then OpenSSL::Digest::SHA384
+        when :sha512 then OpenSSL::Digest::SHA512
         else
-          Digest::SHA1
+          OpenSSL::Digest::SHA1
         end
       end
       @algorithm
@@ -79,75 +79,10 @@ module SamlIdp
         # This needs to be better defined, but aiming for my needs at moment
         idp_uri = opts[:idp_uri] || (defined?(request) && "https://#{request.host_with_port}/") || "http://idp.example.com/"
 
-        assertion = <<ASSERTION
-<saml:Assertion
-	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-	xmlns:xs="http://www.w3.org/2001/XMLSchema"
-	ID="_#{assertion_id}"
-	Version="2.0"
-	IssueInstant="#{now.iso8601}">
-    <saml:Issuer>#{idp_uri}</saml:Issuer>
-    <saml:Subject>
-      <saml:NameID
-	      SPNameQualifier="#{request_issuer_name}"
-	      Format="urn:oasis:names:tc:SAML:2.0:nameid-format:transient">_#{request_issuer_id}</saml:NameID>
-      <saml:SubjectConfirmation Method="urn:oasis:names:tc:SAML:2.0:cm:bearer">
-        <saml:SubjectConfirmationData
-		      NotOnOrAfter="#{after_now.iso8601}"
-		      Recipient="#{request_acs_url}"
-		      InResponseTo="#{request_id}"/>
-      </saml:SubjectConfirmation>
-    </saml:Subject>
-    <saml:Conditions NotBefore="#{before_now.iso8601}" NotOnOrAfter="#{after_now.iso8601}">
-      <saml:AudienceRestriction>
-        <saml:Audience>#{request_issuer_name}</saml:Audience>
-      </saml:AudienceRestriction>
-    </saml:Conditions>
-    <saml:AuthnStatement
-	    AuthnInstant="#{now.iso8601}"
-	    SessionNotOnOrAfter="#{timeout.iso8601}"
-	    SessionIndex="_#{session_id}">
-      <saml:AuthnContext>
-        <saml:AuthnContextClassRef>urn:oasis:names:tc:SAML:2.0:ac:classes:Password</saml:AuthnContextClassRef>
-      </saml:AuthnContext>
-    </saml:AuthnStatement>
-    <saml:AttributeStatement>
-      <saml:Attribute Name="firstName" NameFormat="urn:oasis:names:tc:SAML:2.0:attrname-format:uri">
-        <saml:AttributeValue xsi:type="xs:string">#{opts[:first_name]}</saml:AttributeValue>
-      </saml:Attribute>
-      <saml:Attribute Name="lastName" NameFormat="urn:oasis:names:tc:SAML:2.0:attrname-format:uri">
-        <saml:AttributeValue xsi:type="xs:string">#{opts[:last_name]}</saml:AttributeValue>
-      </saml:Attribute>
-      <saml:Attribute Name="email" NameFormat="urn:oasis:names:tc:SAML:2.0:attrname-format:uri">
-        <saml:AttributeValue xsi:type="xs:string">#{opts[:email]}</saml:AttributeValue>
-      </saml:Attribute>
-      <saml:Attribute Name="externalId" NameFormat="urn:oasis:names:tc:SAML:2.0:attrname-format:uri">
-        <saml:AttributeValue xsi:type="xs:string">#{opts[:account_id]}</saml:AttributeValue>
-      </saml:Attribute>
-    </saml:AttributeStatement>
-  </saml:Assertion>
-ASSERTION
-
+        assertion = %[<saml:Assertion xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xs="http://www.w3.org/2001/XMLSchema" ID="_#{assertion_id}" Version="2.0" IssueInstant="#{now.iso8601}"><saml:Issuer>#{idp_uri}</saml:Issuer><saml:Subject><saml:NameID SPNameQualifier="#{request_issuer_name}" Format="urn:oasis:names:tc:SAML:2.0:nameid-format:transient">_#{request_issuer_id}</saml:NameID><saml:SubjectConfirmation Method="urn:oasis:names:tc:SAML:2.0:cm:bearer"><saml:SubjectConfirmationData NotOnOrAfter="#{after_now.iso8601}" Recipient="#{request_acs_url}" InResponseTo="#{request_id}"/></saml:SubjectConfirmation></saml:Subject><saml:Conditions NotBefore="#{before_now.iso8601}" NotOnOrAfter="#{after_now.iso8601}"><saml:AudienceRestriction><saml:Audience>#{request_issuer_name}</saml:Audience></saml:AudienceRestriction></saml:Conditions><saml:AuthnStatement AuthnInstant="#{now.iso8601}" SessionNotOnOrAfter="#{timeout.iso8601}" SessionIndex="_#{session_id}"><saml:AuthnContext><saml:AuthnContextClassRef>urn:oasis:names:tc:SAML:2.0:ac:classes:Password</saml:AuthnContextClassRef></saml:AuthnContext></saml:AuthnStatement><saml:AttributeStatement><saml:Attribute Name="firstName" NameFormat="urn:oasis:names:tc:SAML:2.0:attrname-format:uri"><saml:AttributeValue xsi:type="xs:string">#{opts[:first_name]}</saml:AttributeValue></saml:Attribute><saml:Attribute Name="lastName" NameFormat="urn:oasis:names:tc:SAML:2.0:attrname-format:uri"><saml:AttributeValue xsi:type="xs:string">#{opts[:last_name]}</saml:AttributeValue></saml:Attribute><saml:Attribute Name="email" NameFormat="urn:oasis:names:tc:SAML:2.0:attrname-format:uri"><saml:AttributeValue xsi:type="xs:string">#{opts[:email]}</saml:AttributeValue></saml:Attribute><saml:Attribute Name="externalId" NameFormat="urn:oasis:names:tc:SAML:2.0:attrname-format:uri"><saml:AttributeValue xsi:type="xs:string">#{opts[:account_id]}</saml:AttributeValue></saml:Attribute></saml:AttributeStatement></saml:Assertion>]
         signature = create_signature(assertion, assertion_id)
         assertion_and_signature = assertion.sub(/\<saml:Subject/, "#{signature}<saml:Subject")
-
-        response = <<RESPONSE
-<samlp:Response
-    xmlns:samlp="urn:oasis:names:tc:SAML:2.0:protocol"
-	  xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion"
-	  ID="_#{response_id}"
-    Version="2.0"
-    IssueInstant="#{now.iso8601}"
-    Destination="#{@saml_acs_url}"
-    InResponseTo="#{@saml_request_id}">
-  <saml:Issuer>#{idp_uri}</saml:Issuer>
-  <samlp:Status>
-    <samlp:StatusCode Value="urn:oasis:names:tc:SAML:2.0:status:Success" />
-  </samlp:Status>
-  #{assertion_and_signature}
-</samlp:Response>
-RESPONSE
-
+        response = %[<samlp:Response xmlns:samlp="urn:oasis:names:tc:SAML:2.0:protocol" xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion" ID="_#{response_id}" Version="2.0" IssueInstant="#{now.iso8601}" Destination="#{@saml_acs_url}" InResponseTo="#{@saml_request_id}"><saml:Issuer>#{idp_uri}</saml:Issuer><samlp:Status><samlp:StatusCode Value="urn:oasis:names:tc:SAML:2.0:status:Success"/></samlp:Status>#{assertion_and_signature}</samlp:Response>]
         signature = create_signature(response, response_id)
         signed = response.sub(/\<samlp:Status/, "#{signature}<samlp:Status")
 
@@ -166,7 +101,7 @@ RESPONSE
 
     def sign(data)
       key = OpenSSL::PKey::RSA.new(self.secret_key)
-      Base64.encode64(key.sign(OpenSSL::Digest::SHA1.new, data))
+      Base64.encode64(key.sign(algorithm.new, data))
     end
   end
 end
